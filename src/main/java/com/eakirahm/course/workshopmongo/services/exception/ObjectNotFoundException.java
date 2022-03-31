c617f48cbf88c3db067faeb84e0338d4d1c69b6f
eakirahm <eakirahm@gmail.com>
2022-03-31 20:54:25 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/eakirahm/course/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.eakirahm.course.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
